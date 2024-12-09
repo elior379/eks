6143de1b26d759d3a9d1b7ac4e7819e6e63e70c6
Elior Farajpur <elior379@gmail.com>
2024-12-09 11:34:43 +0200

    eks

--- eks.tf
+++ eks.tf
@@ -1,29 +1,29 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "20.31.0"
 
   cluster_name                             = var.cluster_name
   cluster_version                          = var.cluster_version
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
-  cluster_addons = {
-    vpc-cni = {
-      before_compute = true
-      most_recent    = true
-      configuration_values = jsonencode({
-        env = {
-          ENABLE_POD_ENI                    = "true"
-          ENABLE_PREFIX_DELEGATION          = "true"
-          POD_SECURITY_GROUP_ENFORCING_MODE = "standard"
-        }
-        nodeAgent = {
-          enablePolicyEventLogs = "true"
-        }
-        enableNetworkPolicy = "true"
-      })
-    }
-  }
+  # cluster_addons = {
+  #   vpc-cni = {
+  #     before_compute = true
+  #     most_recent    = true
+  #     configuration_values = jsonencode({
+  #       env = {
+  #         ENABLE_POD_ENI                    = "true"
+  #         ENABLE_PREFIX_DELEGATION          = "true"
+  #         POD_SECURITY_GROUP_ENFORCING_MODE = "standard"
+  #       }
+  #       nodeAgent = {
+  #         enablePolicyEventLogs = "true"
+  #       }
+  #       enableNetworkPolicy = "true"
+  #     })
+  #   }
+  # }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
@@ -33,19 +33,19 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      instance_types           = ["m5.large"]
+      instance_types           = ["m7i.large"]
       force_update_version     = true
       release_version          = var.ami_release_version
       use_name_prefix          = false
       iam_role_name            = "${var.cluster_name}-ng-default"
       iam_role_use_name_prefix = false
 
-      min_size     = 3
-      max_size     = 6
-      desired_size = 3
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
 
       update_config = {
-        max_unavailable_percentage = 50
+        max_unavailable_percentage = 100 // Only for cost savings
       }
 
       labels = {
